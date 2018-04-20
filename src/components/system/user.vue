a8beac610d3f4f1169a4f68f2a9b6a55bf250d34
liuxueliang <liuxueliang@yingu.com>
2018-04-20 11:40:09 +0800

    编辑、删除弹出框

--- src/components/system/user.vue
+++ src/components/system/user.vue
@@ -32,8 +32,8 @@
       <el-table-column fixed="left" label="操作" width="150">
         <template slot-scope="scope">
           <el-button @click="openDetailDialog(scope.row)" type="text" size="small">查看</el-button>
-          <el-button type="text" size="small">编辑</el-button>
-          <el-button type="text" size="small">删除</el-button>
+          <el-button type="text" size="small" @click="openModifyDialog(scope.row)">编辑</el-button>
+          <el-button type="text" size="small" @click="openDeleteDialog(scope.row)">删除</el-button>
         </template>
       </el-table-column>
       <el-table-column prop="nickName" label="用户名" width="150" fixed></el-table-column>
@@ -59,7 +59,7 @@
       </el-pagination>
     </div>
 
-    <!-- 对话框 新增 开始-->
+    <!-- 【新增】对话框 开始-->
     <el-dialog title="新增" :visible.sync="saveDialogVisible" @close="closeSaveDialog">
       <el-row :gutter="20" style="margin-top: 10px;">
         <el-col :span="12">
@@ -112,7 +112,7 @@
           <el-input placeholder="邮箱" v-model="saveParam.email"/>
         </el-col>
         <el-col :span="12">
-          <el-select v-model="saveParam.roles" filterable multiple placeholder="角色" style="width: 100%;">
+          <el-select v-model="roles" filterable multiple placeholder="角色" style="width: 100%;">
             <el-option
               v-for="item in options.roles"
               :key="item.value"
@@ -127,9 +127,9 @@
         <el-button type="primary" @click="saveDialogCommit">确 定</el-button>
       </div>
     </el-dialog>
-    <!-- 对话框 新增 结束-->
+    <!-- 【新增】对话框 结束-->
 
-    <!-- 对话框 查看 开始-->
+    <!-- 【查看】对话框 开始-->
     <el-dialog title="查看" :visible.sync="detailDialogVisible" width="60%" @close="closeDetailDialog">
       <el-row :gutter="20" class="detail-row">
         <el-col :span="3" class="detail-head">ID：</el-col>
@@ -168,7 +168,87 @@
         <el-col :span="9" class="detail-content">{{detailParam.roles || '-'}}</el-col>
       </el-row>
     </el-dialog>
-    <!-- 对话框 查看 结束-->
+    <!-- 【查看】对话框 结束-->
+
+    <!-- 【编辑】对话框 开始-->
+    <el-dialog title="编辑" :visible.sync="modifyDialogVisible" @close="closeModifyDialog">
+      <el-row :gutter="20" style="margin-top: 10px;">
+        <el-col :span="12">
+          <el-input placeholder="用户名" v-model="modifyParam.nickName"/>
+        </el-col>
+        <el-col :span="12">
+          <el-input placeholder="姓名" v-model="modifyParam.realName"/>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20" style="margin-top: 10px;">
+        <el-col :span="12">
+          <el-input placeholder="手机号" v-model="modifyParam.phone"/>
+        </el-col>
+        <el-col :span="12">
+          <el-input placeholder="身份证号" v-model="modifyParam.idNumber"/>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20" style="margin-top: 10px;">
+        <el-col :span="12">
+          <el-select v-model="modifyParam.province" filterable placeholder="省份" style="width: 100%;">
+            <el-option
+              v-for="item in options.province"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </el-col>
+        <el-col :span="12">
+          <el-select v-model="modifyParam.city" filterable placeholder="城市" style="width: 100%;">
+            <el-option
+              v-for="item in options.city"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20" style="margin-top: 10px;">
+        <el-col :span="12">
+          <el-input placeholder="微信号" v-model="modifyParam.wechat"/>
+        </el-col>
+        <el-col :span="12">
+          <el-input placeholder="QQ号" v-model="modifyParam.qq"/>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20" style="margin-top: 10px;">
+        <el-col :span="12">
+          <el-input placeholder="邮箱" v-model="modifyParam.email"/>
+        </el-col>
+        <el-col :span="12">
+          <el-select v-model="roles" filterable multiple placeholder="角色" style="width: 100%;">
+            <el-option
+              v-for="item in options.roles"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </el-col>
+      </el-row>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="modifyDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="modifyDialogCommit">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 【编辑】对话框 结束-->
+
+    <!-- 【删除】对话框 开始-->
+    <el-dialog title="提示" :visible.sync="deleteDialogVisible" @close="closeDeleteDialog" width="30%">
+      <span>确定删除该用户为「{{deleteParam.nickName}}」的用户？</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="deleteDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="deleteDialogCommit">确 定</el-button>
+      </span>
+    </el-dialog>
+    <!-- 【删除】对话框 开始-->
   </div>
 </template>
 
@@ -183,6 +263,7 @@ export default {
   name: 'Index',
   data () {
     return {
+      roles: [],
       options: {
         province: [
           {value: '01',label: '北京'},
@@ -240,11 +321,28 @@ export default {
         roles: '',
         createTime: ''
       },
+      modifyParam: {
+        nickName: '',
+        realName: '',
+        phone: '',
+        idNumber: '',
+        province: '',
+        city: '',
+        wechat: '',
+        qq: '',
+        email: '',
+        roles:''
+      },
+      deleteParam: {
+        nickName: ''
+      },
       tableData: [],
       recordsTotal: 0,
       currentPage4: 4,
       saveDialogVisible: false,
+      modifyDialogVisible: false,
       detailDialogVisible: false,
+      deleteDialogVisible: false,
       form: {
         name: '',
         region: '',
@@ -288,36 +386,6 @@ export default {
     openSaveDialog(){
       this.saveDialogVisible = true
     },
-    saveDialogCommit(){
-      var that = this;
-      if(that.saveParam.roles){
-        that.saveParam.roles = that.saveParam.roles.join(',');
-      }
-      var url = basePath + 'sysUser/save';
-      this.$http.post(url, that.saveParam).then((response) => {
-        console.log(response);
-        this.closeSaveDialog();
-        this.search();
-      }, (response) => {
-        if(that.saveParam.roles){
-          that.saveParam.roles = that.saveParam.roles.split(',');
-        }
-        console.log("err"+response);
-      });
-    },
-    closeSaveDialog(){
-      this.saveParam.nickName = '';
-      this.saveParam.realName = '';
-      this.saveParam.phone = '';
-      this.saveParam.idNumber = '';
-      this.saveParam.province = '';
-      this.saveParam.city = '';
-      this.saveParam.wechat = '';
-      this.saveParam.qq = '';
-      this.saveParam.email = '';
-      this.saveParam.roles = '';
-      this.saveDialogVisible =false;
-    },
     openDetailDialog(row) {
       console.log("row:"+row);
       this.detailParam.id = row.id;
@@ -332,9 +400,94 @@ export default {
       this.detailParam.wechat = row.wechat;
       this.detailParam.qq = row.qq;
       this.detailParam.email = row.email;
-      this.detailParam.roles = row.roles;
+      this.detailParam.roles = this.rolesUtil(row.roles);
       this.detailParam.createTime = row.createTime;
       this.detailDialogVisible = true;
+    },
+    rolesUtil(values){
+      var that = this;
+      if(!values){
+        return null;
+      }
+      var roleArr = values.split(',');
+      var texts = "";
+      for(var i=0;i<roleArr.length;i++){
+        var key = roleArr[i];
+        if(that.options.roles){
+          for(var j=0;j<that.options.roles.length;j++){
+            var roleObj = that.options.roles[j];
+            if(roleObj.value == key){
+              var text = roleObj.label;
+              texts += text;
+              if(i<roleArr.length-1){
+                texts += ",";
+              }
+            }
+          }
+        }
+      }
+      return texts;
+    },
+    openModifyDialog(row){
+      this.modifyParam.id = row.id;
+      this.modifyParam.code = row.code;
+      this.modifyParam.nickName = row.nickName;
+      this.modifyParam.realName = row.realName;
+      this.modifyParam.password = row.password;
+      this.modifyParam.phone = row.phone;
+      this.modifyParam.idNumber = row.idNumber;
+      this.modifyParam.province = row.province;
+      this.modifyParam.city = row.city;
+      this.modifyParam.wechat = row.wechat;
+      this.modifyParam.qq = row.qq;
+      this.modifyParam.email = row.email;
+      this.modifyParam.roles = row.roles;
+      if(row.roles){
+        this.roles = row.roles.split(',');
+      }
+      this.modifyParam.createTime = row.createTime;
+      this.modifyDialogVisible = true
+    },
+    openDeleteDialog(row){
+      this.deleteParam.code = row.code;
+      this.deleteParam.nickName = row.nickName;
+      this.deleteDialogVisible = true;
+    },
+    saveDialogCommit(){
+      var that = this;
+      if(that.roles){
+        that.saveParam.roles = that.roles.join(',');
+      }
+      var url = basePath + 'sysUser/save';
+      this.$http.post(url, that.saveParam).then((response) => {
+        console.log(response);
+        this.closeSaveDialog();
+        this.search();
+      }, (response) => {
+        if(that.saveParam.roles){
+          that.saveParam.roles = that.saveParam.roles.split(',');
+        }
+        console.log("err"+response);
+      });
+    },
+    modifyDialogCommit(){
+      alert("modifyDialogCommit");
+    },
+    deleteDialogCommit(){
+      alert("deleteDialogCommit");
+    },
+    closeSaveDialog(){
+      this.saveParam.nickName = '';
+      this.saveParam.realName = '';
+      this.saveParam.phone = '';
+      this.saveParam.idNumber = '';
+      this.saveParam.province = '';
+      this.saveParam.city = '';
+      this.saveParam.wechat = '';
+      this.saveParam.qq = '';
+      this.saveParam.email = '';
+      this.roles = [];
+      this.saveDialogVisible =false;
     },
     closeDetailDialog(){
       this.detailParam.id = '';
@@ -350,8 +503,31 @@ export default {
       this.detailParam.qq = '';
       this.detailParam.email = '';
       this.detailParam.roles = '';
+      this.roles = '';
       this.detailParam.createTime = '';
       this.detailDialogVisible = false;
+    },
+    closeModifyDialog(){
+      this.modifyParam.id = '';
+      this.modifyParam.code = '';
+      this.modifyParam.nickName = '';
+      this.modifyParam.realName = '';
+      this.modifyParam.password = '';
+      this.modifyParam.phone = '';
+      this.modifyParam.idNumber = '';
+      this.modifyParam.province = '';
+      this.modifyParam.city = '';
+      this.modifyParam.wechat = '';
+      this.modifyParam.qq = '';
+      this.modifyParam.email = '';
+      this.modifyParam.roles = '';
+      this.roles = '';
+      this.modifyParam.createTime = '';
+      this.modifyDialogVisible =false;
+    },
+    closeDeleteDialog(){
+      this.deleteParam.code = '';
+      this.deleteParam.nickName = '';
     }
   },
   mounted: function () {
